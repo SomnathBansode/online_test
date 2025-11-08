4428d970f265ec02c547c457d69710c798ed5136
SomnathBansode <bansodesomnath888@gmail.com>
2025-11-09 02:56:06 +0530

    reset password done demo

--- online-test-frontend/src/prop-types.js
+++ online-test-frontend/src/prop-types.js
@@ -1,9 +1,9 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 export const loaderPropTypes = {
   size: PropTypes.number,
   color: PropTypes.string,
   className: PropTypes.string,
   strokeWidth: PropTypes.number,
-  message: PropTypes.string
+  message: PropTypes.string,
 };
